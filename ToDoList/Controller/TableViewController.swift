3e56d08a6b2d5aad3c191c798cd1b64cf4108825
Hussain AlMayad <hussainp1411@gmail.com>
2018-03-02 18:49:30 +0300

    Modify Files Structure to Follow MVC

---
+++ ToDoList/Controller/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Hussain AlMaiyed on 02/03/2018.
+//  Copyright © 2018 Hussain AlMayad. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
